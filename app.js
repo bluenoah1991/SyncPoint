742bed91e9007d74817cf8cedf9832698707bbba
CodeMeow5 <codemeow@ieee.org>
2017-10-30 15:25:30 +0800

    [Important] Fix resolving conflicts error

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const SyncPoint = require('./index');
 var opts = {
     'resolvingConflicts': function(serverPoints, newPoints, numberOfSegment){
         var conflictsPoints = [];
+        var reversePoints = [];
         for(var i in newPoints){
             var point = newPoints[i];
             point.syncNumber = ++numberOfSegment;
             conflictsPoints.push(point);
         }
-        return conflictsPoints;
+        for(var i in serverPoints){
+            var serverPoint = serverPoints[i];
+            reversePoints.push(serverPoint);
+        }
+        return [conflictsPoints, reversePoints];
     },
     'httpport': 8080
 };
